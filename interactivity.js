fa12993354947595618cdc3f907133d16adcfe7f
Meher Yar Khan <meheryarkhan90@hotmail.com>
2018-03-22 18:39:37 +0200

    Add files via upload
    Fleshing out the basics of the App.

---
+++ interactivity.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#btn_show-add-obs-section').click(function(){
+        $('#add-obs').toggle();
+    });
+});
+
+/*
+
+function showAddObservationSection() {
+    var x = document.getElementById("add-obs");
+    var b = document.getElementById("btn_show-add-obs-section");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        b.style.display = "none";
+    } else {
+        x.style.display =  "none";
+    }
+} 
+
+
+*/
